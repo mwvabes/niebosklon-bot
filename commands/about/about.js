4a4ce28c23a3b4549096f94313bf8c0bc34e7134
Marcin <kontakt.vabes@gmail.com>
2018-06-15 23:05:02 +0200

    About

--- commands/about/about.js
+++ commands/about/about.js
@@ -14,15 +14,12 @@ class AboutCommand extends commando.Command {
         
         
         
-        var msg = "Pod Nieboskłonem cienia \n \n \n Śmigam na Heroku! | ver 1.0.0";
+        var msg = "Pod Nieboskłonem cienia \n \n \nJestem Bot Nieboskłon, śmigam na Heroku! \n&copy; 2018 by vabes.pl | ver 1.0.0";
         
         message.channel.send({embed: {
           color: 3447003,
           description: msg
         }});
-        
-/*        var embed = new commando.RichEmbed().setDescription(msg);
-        message.channel.sendEmbed(embed);*/
         
 
     }
