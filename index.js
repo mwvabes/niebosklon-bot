cd0de641e1bb845812eb1d72f4b332890afbfcc6
Marcin <kontakt.vabes@gmail.com>
2018-06-15 22:09:40 +0200

    Changing index

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const commando = require('discord.js-commando');
-const bot = new commando.Client();
+const Discord = require('discord.js');
+//const commando = require('discord.js-commando');
+//const bot = new commando.Client();
+const client = new Discord.Client();
 const token = process.env.token;
 
 console.log('Start!');
 
-bot.registry.registerGroup('random', 'Random');
+/*bot.registry.registerGroup('random', 'Random');
 bot.registry.registerGroup('arki', 'Arki');
 bot.registry.registerGroup('basics', 'Basics');
 bot.registry.registerGroup('about', 'about');
@@ -13,4 +15,6 @@ bot.registry.registerCommandsIn(__dirname + "/commands");
 
 
 
-bot.login(token);
+bot.login(token);*/
+
+client.login(token).catch(err => console.log(err));
