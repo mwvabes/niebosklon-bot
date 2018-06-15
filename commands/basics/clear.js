b15e267ed5c11c5490ab9ddf2b58fb8e6aad0ff4
Marcin <kontakt.vabes@gmail.com>
2018-06-15 23:15:02 +0200

    Improved visual aspects

--- commands/basics/clear.js
+++ commands/basics/clear.js
@@ -26,9 +26,9 @@ class ClearCommand extends commando.Command {
 
             const fetched = await message.channel.fetchMessages({limit: 100}); // This grabs the last number(args1) of messages in the channel.
 
-            message.channel.sendMessage("\n \n \n \n  `--------------------------------------");
-            message.channel.sendMessage("Usuwam " + fetched.size + " wiadomości...");
-            message.channel.sendMessage("\n--------------------------------------` \n \n \n \n  ");
+            message.channel.sendMessage("\n \n \n \n  ```--------------------------------------");
+            message.channel.sendMessage("**Usuwam " + fetched.size + " wiadomości...**");
+            message.channel.sendMessage("\n--------------------------------------``` \n \n \n \n  ");
         
             const fetched_new = await message.channel.fetchMessages({limit: 100}); // This grabs the last number(args1) of messages in the channel.
             console.log(fetched_new.size + ' messages found, deleting...'); // Lets post into console how many messages we are deleting
