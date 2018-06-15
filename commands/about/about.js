9336c93a1e105933341fb3b1eaf7f385cbee4970
Marcin <kontakt.vabes@gmail.com>
2018-06-15 23:02:59 +0200

    Embed about

--- commands/about/about.js
+++ commands/about/about.js
@@ -12,10 +12,17 @@ class AboutCommand extends commando.Command {
     
     async run(message, args) {
         
+        
+        
         var msg = "Pod Nieboskłonem cienia \n \n \n Śmigam na Heroku! | ver 1.0.0";
         
-        var embed = new commando.RichEmbed().setDescription(msg);
-        message.channel.sendEmbed(embed);
+        message.channel.send({embed: {
+          color: 3447003,
+          description: msg
+        }});
+        
+/*        var embed = new commando.RichEmbed().setDescription(msg);
+        message.channel.sendEmbed(embed);*/
         
 
     }
