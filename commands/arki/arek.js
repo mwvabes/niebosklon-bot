a0c87153a8b639cd18ddbdba5fa73758d8aabb16
Marcin <kontakt.vabes@gmail.com>
2018-06-21 22:56:46 +0200

    Feed command

--- commands/arki/arek.js
+++ commands/arki/arek.js
@@ -20,4 +20,21 @@ class ArekCommand extends commando.Command {
     }
 }
 
+
+class FeedCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'feed',
+            group: 'arki',
+            memberName: 'feed',
+            description: 'Karmi bota'
+        });
+    }
+    
+    async run(message, args) {
+        message.channel.sendMessage("Om nom nom nom nom...");
+        
+    }
+}
+
 module.exports = ArekCommand;
